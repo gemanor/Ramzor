a8f2ecd5b9b2fdf77dfdf520b1903b53b4b6861e
Gabriel Manor <g.e.manor@gmail.com>
2020-11-23 15:10:53 +0200

    Client

--- example/src/services/unsplash.js
+++ example/src/services/unsplash.js
@@ -1,6 +1,8 @@
+const clientID = ''; // Enter unsplash client ID for fetching pictures
+
 const Page = async (username, page, size, old = []) => {
     try {
-        const response = await fetch(`https://api.unsplash.com/users/${username}/photos?per_page=${size}&page=${page}&client_id=22ae3a3fb9415b82a2f8da77b560210d4aac32adc0a3fe12dc5b4e2b85ebc384`);
+        const response = await fetch(`https://api.unsplash.com/users/${username}/photos?per_page=${size}&page=${page}&client_id=${clientID}`);
         const status = response.status;
         const total = response.headers.get('X-Total');
         const json = await response.json();
